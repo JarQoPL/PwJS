0eb5699012e907cd032743bed9faab4b383d05ba
JarQoPL <jaroslaw.swiechowski@hotmail.com>
2018-09-30 14:15:39 +0200

    Laborki1

---
+++ Zad2.py
@@ -0,0 +1,3 @@
+rachunek = int(input("wpisz kwotę rachunku"))
+print("Napiwek 15% wynosi: ",round(rachunek*0.15))
+print("Napiwek 20% wynosi: ",round(rachunek*0.20))
---
+++ zad1.py
@@ -0,0 +1,3 @@
+przysmak1=input("Podaj pierwszy smaczek: ")
+przysmak2=input("Podaj drugi smaczek: ")
+print(przysmak1+przysmak2)
