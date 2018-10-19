1fc28204c8170c1396795464eed91aa30f4a0b8b
JarQoPL <43720509+JarQoPL@users.noreply.github.com>
2018-10-19 21:29:08 +0200

    Delete Zad3.py

--- Zad3.py
+++
@@ -1,6 +0,0 @@
-cena = float(input("wprowadź cenę samochodu "))
-podatek = cena*0.19
-oplataRejestracyjna = float(cena*0.05)
-prowizja = 1200
-dostawa = 200
-print("cena samochodu razem ", cena+podatek+oplataRejestracyjna+dostawa+prowizja)
